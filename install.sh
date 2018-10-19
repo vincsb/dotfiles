db06addcd029db0c59b9eb579fa0ab301946ca3f
vincsb <vincent.bocquet@live.fr>
2018-10-19 15:38:38 +0200

    feat(install): Move atom packages list to install bash script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Install atom packages
+apm install atom-beautify
+apm install atom-clock
+apm install atom-material-syntax
+apm install atom-spotify2
+apm install autocomplete-paths
+apm install emmet
+apm install expand-region
+apm install file-icons
+apm install git-blame
+apm install git-plus
+apm install highlight-selected
+apm install language-javascript-jsx
+apm install linter
+apm install linter-eslint
+apm install project-manager
